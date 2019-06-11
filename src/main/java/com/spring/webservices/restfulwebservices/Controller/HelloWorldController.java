e10fb798e995e5975ca62d8a1e6ee10af2175b67
nitin023 <khulbe.nitin@gmail.com>
2019-06-12 00:17:04 +0530

    internationalization of api

--- src/main/java/com/spring/webservices/restfulwebservices/Controller/HelloWorldController.java
+++ src/main/java/com/spring/webservices/restfulwebservices/Controller/HelloWorldController.java
@@ -1,12 +1,21 @@
 package com.spring.webservices.restfulwebservices.Controller;
 
 import com.spring.webservices.restfulwebservices.DTO.HelloWorldBean;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    private MessageSource messageSource;
 
     @GetMapping(path="/hello-world")
     public String helloWorld()
@@ -24,5 +33,11 @@ public class HelloWorldController {
     public HelloWorldBean helloWorldBean(@PathVariable String name,@PathVariable String age)
     {
         return new HelloWorldBean("Hello world ," + name + " age " + age);
+    }
+
+    @GetMapping(path="/hello-world-internalisation")
+    public String helloWorldInternalisation(@RequestHeader(value = "Accept-Language",required = false) Locale locale)
+    {
+        return messageSource.getMessage("good.morning.message",null,locale);
     }
 }
--- src/main/java/com/spring/webservices/restfulwebservices/RestfulWebServicesApplication.java
+++ src/main/java/com/spring/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -1,7 +1,13 @@
 package com.spring.webservices.restfulwebservices;
 
-import org.springframework.boot.SpringApplication;
+import  org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class RestfulWebServicesApplication {
@@ -9,5 +15,21 @@ public class RestfulWebServicesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RestfulWebServicesApplication.class, args);
 	}
+
+	@Bean
+	public LocaleResolver  localeResolver()
+    {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.US);
+        return localeResolver;
+    }
+
+    @Bean
+    public ResourceBundleMessageSource bundleMessageSource()
+    {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages");
+        return messageSource;
+    }
 
 }
