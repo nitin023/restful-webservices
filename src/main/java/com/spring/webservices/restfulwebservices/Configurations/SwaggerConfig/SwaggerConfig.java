519c3eeb8e39b9703b3644f745554b6d65471b43
nitin023 <khulbe.nitin@gmail.com>
2019-06-13 23:14:50 +0530

    swagger information update

--- src/main/java/com/spring/webservices/restfulwebservices/Configurations/SwaggerConfig/SwaggerConfig.java
+++ src/main/java/com/spring/webservices/restfulwebservices/Configurations/SwaggerConfig/SwaggerConfig.java
@@ -2,6 +2,8 @@ package com.spring.webservices.restfulwebservices.Configurations.SwaggerConfig;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -10,9 +12,14 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 
+    public static final Contact DEFAULT_CONTACT = new Contact("Nitin","https://github.com/nitin023","khulbe.nitin@gmail.com");
+
+    public static final ApiInfo DEFAULT = new ApiInfo("Api Title", "Api Documentation", "1.0",
+            "urn:tos", DEFAULT_CONTACT, "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0");
+
     @Bean
     public Docket api()
     {
-        return new Docket(DocumentationType.SWAGGER_2);
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(DEFAULT);
     }
 }
